579b8f9874d851f892ec33952adc3ec10888c73d
robertlcook <robertlewiscook@gmail.com>
2022-03-05 20:35:59 -0500

    html moved to react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SignOn from './components/SignOnPage';
+
+function App() {
+  return (
+    <div className="App">
+   < SignOn />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SignOnPage.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  BrowserRouter as Router, Switch,
+  Route, Redirect,
+} from "react-router-dom";
+import '../stylesheet.css';
+import bluePiggyBank from '../images/blue piggy bank.jpg';
+const SignOn = () => {
+  return (
+    <div>
+      <title>Budget App</title>
+      <h1 className="title">Budget App</h1>
+      <form action="action_page.php" method="post">
+        <div class="imgcontainer">
+          <img src= {bluePiggyBank}class="pig" />
+        </div>
+        <div className="container">
+          <label for="uname"><b></b></label>
+          <input type="text" placeholder="Enter Username" name="username" required />
+
+            <label for="psw"><b></b></label>
+            <input type="password" placeholder="Enter Password" name="password" required />
+
+              <button type="submit">Login</button>
+              <label>
+                <input type="checkbox" checked="checked" name="remember" /> Remember me
+              </label>
+            </div>
+          </form>
+        </div>
+        );
+}; export default SignOn;
